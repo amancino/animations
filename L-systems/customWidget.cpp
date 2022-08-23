a4e5626071029aa5bf769ff4b430bf77838316fe
amancino <axel.mancino@gmail.com>
2022-08-24 00:09:06 +0200

    first attempt to draw tree-like system

--- L-systems/customWidget.cpp
+++ L-systems/customWidget.cpp
@@ -9,6 +9,8 @@
 #include <QPen>
 #include <QPushButton>
 
+#include <math.h>
+
 using namespace std;
 
 customWidget::customWidget(QWidget* parent) :
@@ -24,26 +26,33 @@ customWidget::customWidget(QWidget* parent) :
     this->update();
   });
 
-  m_code = "ABA";
+  m_code = "F";
 }
 
 void customWidget::processNewGeneration()
 {
-  std::string newCode;
+  std::string newCode("");
 
   for (char c : m_code)
   {
-    if (c == 'A')
+    switch (c)
     {
-      newCode.append("ABA");
-    }
-    else if (c == 'B')
-    {
-      newCode.append("BBB");
+    case 'F':
+      newCode.append("FF+[+F-F-F]-[-F+F+F]");
+      //newCode.append("F[+F]F[−F]F");
+      break;
     }
   }
 
   m_code = newCode;
+}
+
+void drawGenerations(int gen)
+{
+  for (int g=0; g<gen; ++g)
+  {
+
+  }
 }
 
 void customWidget::paintEvent(QPaintEvent*)
@@ -52,14 +61,41 @@ void customWidget::paintEvent(QPaintEvent*)
   QPainter painter(this);
 
   // move to the bottom and change coordinates to point upwards
-  painter.setPen(Qt::GlobalColor::transparent);
+  //painter.setPen(Qt::GlobalColor::transparent);
   painter.translate(width()/2,height()*0.9);
   painter.rotate(180);
 
   painter.setPen(QPen(Qt::GlobalColor::black,2));
   //painter.drawRoundRect(-rad,-10,2*rad,20,10,20);
 
-  painter.drawLine(0, 0, 0, 40);
+  int alpha = 20;
+  int L = 20 * std::pow(0.7,(m_generation-1));
+
+  for (char c : m_code)
+  {
+    switch (c)
+    {
+    case 'F':
+      painter.drawLine(0,0,0,L);
+      painter.translate(0, L);
+      break;
+    case 'G':
+      painter.translate(0, L);
+      break;
+    case '+':
+      painter.rotate(alpha);
+      break;
+    case '-':
+      painter.rotate(-alpha);
+      break;
+    case '[':
+      painter.save();
+      break;
+    case ']':
+      painter.restore();
+      break;
+    }
+  }
 }
 
 #endif // CUSTOMWIDGET_H
